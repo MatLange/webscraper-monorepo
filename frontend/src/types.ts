9d3ee4182107f28f4e6f0dcdd9173d69ec9bf87b
MatLange <matti.lange@gmx.de>
2024-08-13 14:22:36 +0200

    Added react frontend and make it a monorepo

---
+++ frontend/src/types.ts
@@ -0,0 +1,9 @@
+export type Book = {
+  websiteid: string | null;
+  title: string | null;
+  baseUrl: string | null;
+  url: string | null;
+  location: string | null;
+  date: string | null;
+  duration: string | null;
+}
