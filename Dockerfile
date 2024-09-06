0636fab41e95c15b13162397b2439e63e14727f6
MatLange <matti.lange@gmx.de>
2024-09-07 00:32:48 +0200

    Update port

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN npm install --production
 COPY ./dist ./dist
 
 # Expose the port the app runs on.
-EXPOSE 3000
+EXPOSE 8080
 
 # Run the web service on container startup.
 CMD [ "npm", "start:prod" ]
--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ const start = async () => {
    * Get port from environment and store in Express.
    */
 
-  const port = normalizePort(process.env.PORT || "3000");
+  const port = normalizePort(process.env.PORT || "8080");
   app.set("port", port);
  
   /**
